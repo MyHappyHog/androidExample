6fd862060aff85999b39c84a6db9bd0bee40fbec
gyu-hyeon <ngh3053@gmail.com>
2015-11-23 00:11:16 +0900

    custom listview run in navigation drawer
    when item is clicked in listview, pop up the toast.

--- ToolbarTest/app/src/main/java/h3/com/toolbartest/MainActivity.java
+++ ToolbarTest/app/src/main/java/h3/com/toolbartest/MainActivity.java
@@ -1,13 +1,24 @@
 package h3.com.toolbartest;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.ArrayAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,20 +28,107 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.left_drawer)
     ListView listView;
 
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        String[] numbers = { "1", "2", "3", "44" };
+        final ArrayList<TestItem> arrayList = new ArrayList<>();
+        arrayList.add(new TestItem(android.R.drawable.ic_dialog_alert, "ic_dialog_alert"));
+        arrayList.add(new TestItem(android.R.drawable.ic_dialog_dialer, "ic_dialog_dialer"));
+        arrayList.add(new TestItem(android.R.drawable.ic_dialog_email, "ic_dialog_email"));
+        arrayList.add(new TestItem(android.R.drawable.ic_dialog_info, "ic_dialog_info"));
+        arrayList.add(new TestItem(android.R.drawable.ic_dialog_map, "ic_dialog_map"));
 
-        //ListView listView = (ListView)findViewById(R.id.left_drawer);
-        listView.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.item999, numbers));
-        
+        listView.setAdapter(new TestAdapter(getApplicationContext(), R.layout.list_item, arrayList));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), arrayList.get(position).text, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
+class TestItem {
+    int img;
+    String text;
+
+    public TestItem() {
+
     }
 
+    public TestItem(int img, String text) {
+        this.img = img;
+        this.text = text;
+    }
+}
 
+class TestAdapter extends BaseAdapter {
+    private Context context;
+    private int layout;
+    private ArrayList<TestItem> arrayList;
+    private LayoutInflater layoutInflater;
+
+    public TestAdapter(Context context, int layout, ArrayList<TestItem> arrayList) {
+        this.context = context;
+        this.layout = layout;
+        this.arrayList = arrayList;
+        this.layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(layout, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+
+        viewHolder.imageView.setImageResource((arrayList.get(position)).img);
+        viewHolder.textView.setText((arrayList.get(position)).text);
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        @Bind(R.id.testIcon)
+        ImageView imageView;
+
+        @Bind(R.id.testText)
+        TextView textView;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
 }
